6558d90c3556078b3d591e56c9b3cbf198328e3f
haydenmlh <haydenmlh@gmail.com>
2020-08-23 14:13:45 -0400

    Part of Step 2

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Miguelo'}
     return render_template('index.html', title='Home', user=user)
